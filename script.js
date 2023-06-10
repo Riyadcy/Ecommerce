89a47a3dce4ccbd642f0bd68330d66ee2ad56fd2
Riyad C <73402507+Riyadcy@users.noreply.github.com>
2023-06-10 06:36:44 +0530

    "Grid"

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ fetch("https://fakestoreapi.com/products").then((data)=>{
 }).then((objectData)=>{
     let tableData=" ";
     objectData.map((value)=>{
-        tableData+=  `<img src="${value.image}" class="card-img-top" alt="${value.title}">
-        <div class="card-body">
-            <h5 class="card-title">${value.title}</h5>
-            <p class="card-text">${value.description}</p>
-            <span>${value.price}</span>
-            <a href="#" class="btn btn-primary">Buy it</a>
+        tableData+=  `
+        <div class="col">
+            <div class="card">
+                <img src="${value.image}" class="card-img-top img-fluid" alt="${value.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${value.title}</h5>
+                    <p class="card-text">${value.description}</p>
+                    <span>${value.price}</span>
+                    <a href="#" class="btn btn-primary">Buy it</a>
+                </div>
+            </div>
         </div>`;
     })
     document.getElementById("cards").innerHTML=tableData;
 })
     
+
+   
